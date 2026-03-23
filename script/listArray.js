e64995fb0f541d04e11b740584544a7869f28fc2
조성현 <sunny1762@naver.com>
2026-03-23 12:43:38 +0900

    리스트

---
+++ script/listArray.js
@@ -0,0 +1,60 @@
+let listArray = [
+    {
+        pid:0,
+        pName:"상품명-1",
+        pDesc: "상품 관련 간단한 설명",
+        pPrice:3000,
+        pThumbnail:"list-1-img.jpg", //파일명 규칙있는 이름으로 설정하기, 예를 들어 list-ooo-ooo.jpg
+        plike: false
+    },
+  {
+        pid:1,
+        pName:"상품명-2",
+        pDesc: "상품 관련 간단한 설명",
+        pPrice:3000,
+        pThumbnail:"list-1-img.jpg", 
+        plike: false
+    },  {
+        pid:2,
+        pName:"상품명-3",
+        pDesc: "상품 관련 간단한 설명",
+        pPrice:3000,
+        pThumbnail:"list-1-img.jpg", 
+        plike: false
+    },  {
+        pid:3,
+        pName:"상품명-4",
+        pDesc: "상품 관련 간단한 설명",
+        pPrice:3000,
+        pThumbnail:"list-1-img.jpg", 
+        plike: false
+    },  {
+        pid:4,
+        pName:"상품명-5",
+        pDesc: "상품 관련 간단한 설명",
+        pPrice:3000,
+        pThumbnail:"list-1-img.jpg", 
+        plike: false
+    },  {
+        pid:5,
+        pName:"상품명-6",
+        pDesc: "상품 관련 간단한 설명",
+        pPrice:3000,
+        pThumbnail:"list-1-img.jpg", 
+        plike: false
+    },  {
+        pid:6,
+        pName:"상품명-7",
+        pDesc: "상품 관련 간단한 설명",
+        pPrice:3000,
+        pThumbnail:"list-1-img.jpg", 
+        plike: false
+    },  {
+        pid:7,
+        pName:"상품명-8",
+        pDesc: "상품 관련 간단한 설명",
+        pPrice:3000,
+        pThumbnail:"list-1-img.jpg", 
+        plike: false
+    },
+]
