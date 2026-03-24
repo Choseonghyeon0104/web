2387a66271116d26872cf4c470412a36d9058029
조성현 <sunny1762@naver.com>
2026-03-24 11:23:33 +0900

    이미지 수정

--- script/listArray.js
+++ script/listArray.js
@@ -1,46 +1,47 @@
 let listArray = [
     {
         pid:0,
-        pName:"상품명-1",
-        pDesc: "상품 관련 간단한 설명",
-        pPrice:3000,
-        pThumbnail:"list-1-img.jpg", //파일명 규칙있는 이름으로 설정하기, 예를 들어 list-ooo-ooo.jpg
+        pName:"그레인프리 데일리 사료",
+        pDesc: "곡물 알러지를 최소화하고 매일 급여해도 질리지 않는 균형 잡힌 주식",
+        pPrice:28000,
+        pThumbnail:"list-dog-1-1.png", //파일명 규칙있는 이름으로 설정하기, 예를 들어 list-ooo-ooo.jpg
         plike: false
     },
   {
         pid:1,
-        pName:"상품명-2",
-        pDesc: "상품 관련 간단한 설명",
-        pPrice:3000,
-        pThumbnail:"list-1-img.jpg", 
+        pName:"소고기 채소 듬뿍 화식",
+        pDesc: "고단백 소고기와 신선한 파프리카, 브로콜리를 다져 넣은 촉촉한 한 끼",
+        pPrice:4500,
+        pThumbnail:"list-dog-1-2.png", 
         plike: false
-    },  {
+    },  
+    {
         pid:2,
-        pName:"상품명-3",
-        pDesc: "상품 관련 간단한 설명",
-        pPrice:3000,
-        pThumbnail:"list-1-img.jpg", 
+        pName:"연어 고구마 무스 화식",
+        pDesc: "피모 건강에 좋은 연어와 달콤한 고구마를 으깨어 만든 부드러운 식사",
+        pPrice:4200,
+        pThumbnail:"list-dog-1-3.png", 
         plike: false
     },  {
         pid:3,
-        pName:"상품명-4",
-        pDesc: "상품 관련 간단한 설명",
-        pPrice:3000,
-        pThumbnail:"list-1-img.jpg", 
+        pName:"오븐 베이크드 치킨 사료",
+        pDesc: "낮은 온도에서 천천히 구워 영양소 파괴를 최소화한 바삭한 건식 사료",
+        pPrice:22000,
+        pThumbnail:"list-dog-1-4.png", 
         plike: false
     },  {
         pid:4,
-        pName:"상품명-5",
-        pDesc: "상품 관련 간단한 설명",
-        pPrice:3000,
-        pThumbnail:"list-1-img.jpg", 
+        pName:"오리 안심 영양 화식",
+        pDesc: "기력 회복에 좋은 오리 안심과 단호박을 섞어 기호성을 높인 화식",
+        pPrice:4800,
+        pThumbnail:"list-dog-1-5.png", 
         plike: false
     },  {
         pid:5,
-        pName:"상품명-6",
-        pDesc: "상품 관련 간단한 설명",
-        pPrice:3000,
-        pThumbnail:"list-1-img.jpg", 
+        pName:"양고기 허브 테린",
+        pDesc: "호주산 양고기와 로즈마리 향이 어우러진 고급스러운 육류 테린",
+        pPrice:5200,
+        pThumbnail:"list-dog-1-6.png", 
         plike: false
     },  {
         pid:6,
